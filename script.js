3a45de83ecaf0be41852ca8de9e22675c7ab3340
nicospz <niko.ep@gmail.com>
2019-02-24 18:40:26 +0900

    Added support for multiple operations and priority for multiplication and division.

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const equalButton = document.querySelector("#equal");
 const pointButton = document.querySelector("#point");
 
 buttons.forEach(button => button.addEventListener("click", function (e) {
+    //Prevents adding more than one operator
+    if ((this.value == "+" ||
+        this.value == "-" ||
+        this.value == "*" ||
+        this.value == "/") &&
+        (display.value[display.value.length - 1] == "+" ||
+            display.value[display.value.length - 1] == "-" ||
+            display.value[display.value.length - 1] == "*" ||
+            display.value[display.value.length - 1] == "/")) {
+        display.value = display.value.slice(0, display.value.length - 1) + this.value;
+        return;
+    }
+    ////
     display.value += this.value;
     return;
 }));
@@ -13,8 +26,8 @@ clearButton.addEventListener("click", function (e) {
     display.value = "";
     return;
 });
-equalButton.addEventListener("click", function () {
-    for (i = 0; "1234567890.".indexOf(display.value[i]) != -1; i++) {}
+/* equalButton.addEventListener("click", function () {
+    for (i = 0; "1234567890.".indexOf(display.value[i]) != -1; i++) { }
     var number2 = "";
     const number1 = parseFloat(display.value.slice(0, i));
     const operator = display.value[i];
@@ -22,19 +35,29 @@ equalButton.addEventListener("click", function () {
     if (!number2 && operator) {
         alert("You need to add a second number!");
     }
+    if (number1[number1.length - 1] == "." || number2[number2.length - 1] == ".") {
+        return;
+    }
     display.value = operate(operator, number1, number2);
     return;
+}); */
+
+equalButton.addEventListener("click", function () {
+    display.value = evaluate(display.value);
+    return;
 });
-pointButton.addEventListener("click", function() {
+
+pointButton.addEventListener("click", function () {
     for (i = 0; "1234567890.".indexOf(display.value[i]) != -1; i++) {
     }
     var number2 = "";
     const number1 = parseFloat(display.value.slice(0, i));
     const operator = display.value[i];
-    number2str = display.value.slice(i + 1);
+    const number2str = display.value.slice(i + 1);
     number2 = parseFloat(number2str)
     if (display.value.indexOf(".") == -1) {
         display.value += ".";
+        return;
     }
     if (operator && number2str.indexOf(".") == -1) {
         display.value += ".";
@@ -44,14 +67,57 @@ pointButton.addEventListener("click", function() {
 });
 
 
+//recursive function that evaluates a expression by separating it into
+// operator and 2 other expressions, which in the base case are just numbers.
+function evaluate(expression) {
+    for (i = 0; "1234567890.".indexOf(expression[i]) != -1; i++) {
+    }
+    var expression2 = "";
+    var expression1 = expression.slice(0, i);
+    var operator = expression[i];
+    expression2 = expression.slice(i + 1);
+    if (expression[expression.length - 1] == "+" ||
+        expression[expression.length - 1] == "-" ||
+        expression[expression.length - 1] == "*" ||
+        expression[expression.length - 1] == "/" ) {
+        alert("You need to add a second number!");
+    }
 
+    // Priority for multiplication and division
+    if (operator == "*" || operator == "/") {
+        // takes the first number from the second expression to evaluate the product 
+        for (i = 0; "1234567890.".indexOf(expression2[i]) != -1; i++) {
+        }
+        // evaluate product
+        expression1 = operate(operator,expression1,expression2.slice(0, i));
+        operator = expression2[i];
+        expression2 = expression2.slice(i + 1);
+        // if the expression ended here, break
+        if (!expression2) {
+            return expression1;
+        }
+        // since we changed expression1 and expression2, we want to evaluate again
+        // and check if the next operation is a product
+        return (evaluate(expression1 + operator + expression2));
+    }
+    // This part of the function is only for sum and substraction
+    // expression1 is always a number
+    // check if expression2 is a number, if it has any operators, then evaluate it as a new expression
+    if (expression2.indexOf("+") != -1 ||
+        expression2.indexOf("*") != -1 ||
+        expression2.indexOf("-") != -1 ||
+        expression2.indexOf("/") != -1) {
+        expression2 = evaluate(expression2);
+    }
+    return (operate(operator, parseFloat(expression1), parseFloat(expression2)));
 
+}
 
 function add(x, y) {
     return x + y;
 }
 
-function subtract(x, y) {
+function substract(x, y) {
     return x - y;
 }
 
@@ -67,7 +133,7 @@ function operate(operator, x, y) {
     if (x && !operator) {
         return x;
     }
-    if(operator && !y) {
+    if (operator && !y) {
         return x + operator;
     }
     if (operator == "+") {
