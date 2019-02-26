4260bda285cace2d68a3c3e2f0643563cbfe6536
nicospz <niko.ep@gmail.com>
2019-02-27 00:16:12 +0900

    Fixed some button issues and now the calculator evaluates strings using algebraic binary tree

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const clearButton = document.querySelector("#clearButton");
 const equalButton = document.querySelector("#equal");
 const pointButton = document.querySelector("#point");
 const parenthesisButton = document.querySelector("#parenthesis");
-
+const minusplusButton = document.querySelector('#minusplus');
 
 
 buttons.forEach(button => button.addEventListener("click", function (e) {
@@ -41,11 +41,32 @@ buttons.forEach(button => button.addEventListener("click", function (e) {
     return;
 }));
 
-clearButton.addEventListener("click", function (e) {
+clearButton.addEventListener("click", function () {
     display.value = "";
     return;
 });
 
+minusplusButton.addEventListener("click", function () {
+    var expression = display.value
+    var lastNumber;
+    if (expression == parseFloat(expression)) {
+        display.value = "(-" + expression;
+    }
+    for (i = expression.length - 1; i >= 0; i--) {
+        if (expression[i] == "+" ||
+            expression[i] == "-" ||
+            expression[i] == "*" ||
+            expression[i] == "/") {
+            if (expression[i - 1] == "(") {
+                display.value = expression.slice(0, i - 1) + expression.slice(i + 1);
+                return;
+            }
+            lastNumber = expression.slice(i + 1);
+            display.value = expression.slice(0, i + 1) + "(-" + lastNumber;
+            return;
+        }
+    }
+})
 
 equalButton.addEventListener("click", function () {
     if (display.value[display.value.length - 1] == "+" ||
@@ -55,6 +76,23 @@ equalButton.addEventListener("click", function () {
         alert("That's not a valid expression!");
         return;
     }
+    //Complete parenthesis
+    var expression = display.value;
+    var parenthesis = 0;
+    for (i = 0; i < expression.length; i++) {
+        if (expression[i] == "(") {
+            parenthesis++;
+            continue
+        }
+        if (expression[i] == ")") {
+            parenthesis--;
+            continue;
+        }
+    }
+    for (i = 0; i < parenthesis; i++) {
+        display.value += ")";
+    }
+    //-----------------------------
     display.value = evaluate(display.value);
     return;
 });
@@ -106,161 +144,133 @@ parenthesisButton.addEventListener('click', function () {
         }
     }
 });
-
 function evaluate(expression) {
-    // first remove useless parenthesis
-    expression = removeUselessParenthesis(expression);
-    // see if the expression is a number
-    if (expression == parseFloat(expression)) {
+    var tree = expressionTree(expression);
+    return evaluateNode(tree._root);
+}
+function Node(data) {
+    this.data = data;
+    this.parent = null;
+    this.children = [];
+}
+function Tree(data) {
+    var node = new Node(data);
+    this._root = node;
+}
+function removeUselessParenthesis(expression) {
+    if (expression.startsWith("(")) {
+        var inParenthesis = 1;
+        for (i = 1; i < expression.length; i++) {
+            if (expression[i] == "(") {
+                inParenthesis++;
+            }
+            if (expression[i] == ")") {
+                inParenthesis--;
+            }
+            if (inParenthesis == 0 && i < expression.length - 1) {
+                break;
+            }
+            if (inParenthesis == 0 && i == expression.length - 1) {
+                return expression.slice(1, expression.length - 1)
+            }
+        }
         return expression;
     }
-    // see if the expression has sums
-    if (noSums(expression)) {
-        // If it doesn't have sums, then it's a product
-        //get the multiplicating factors first
-        var factors = multiplicatingFactors(expression);
-        var result = product(factors);
-        // then get the dividing factors
-        factors = dividingFactors(expression);
-        var result = result / (product(factors));
-        return result;
-    }
-    // if it has sums, sum...
-    var addends = addendsOf(expression);
-    var result = sum(addends);
-    // if it has substractions, substract...
-    var subtrahends = subtrahendsOf(expression);
-    var result = result - (sum(subtrahends));
-
-    return result;
-
+    return expression;
 }
-
-function multiplicatingFactors(expression) {
-    var factors = [];
-    var j = 0;
+function isSum(expression) {
     var inParenthesis = 0;
-    for (i = 0; i <= expression.length; i++) {
-        if (expression[i] == "(") {
-            inParenthesis++;
-            continue
-        }
+    for (i = expression.length - 1; i >= 0; i--) {
         if (expression[i] == ")") {
+            inParenthesis++;
+            continue;
+        }
+        if (expression[i] == "(") {
             inParenthesis--;
             continue;
         }
-        if ((expression[i] == "*" || expression[i] == "/" || !expression[i]) && inParenthesis == 0) {
-            if (j == 0) {
-                var factor = expression.slice(0, i);
-            } else {
-                var factor = expression.slice(j + 1, i);
-            }
-            if (expression[j] == "*" || j==0) {
-                factors.push((factor));
-            }
-            j = i;
+        if ((expression[i] == "+" || expression[i] == "-") && inParenthesis == 0) {
+            return true;
         }
-    }
-    return factors;
-}
-function dividingFactors(expression) {
-    var factors = [];
-    var j = 0;
-    var inParenthesis = 0;
-    for (i = 0; i <= expression.length; i++) {
-        if (expression[i] == "(") {
-            inParenthesis++;
-            continue
-        }
-        if (expression[i] == ")") {
-            inParenthesis--;
-            continue;
-        }
-        if ((expression[i] == "/" || expression[i] == "*" || !expression[i]) && inParenthesis == 0) {
-            if (expression[j] == "/") {
-                var factor = expression.slice(j + 1, i);
-                factors.push((factor));
-            }
-            j = i;
-        }
-    }
-    return factors;
-}
-function addendsOf(expression) {
-    var addends = [];
-    var j = 0;
-    var inParenthesis = 0;
-    for (i = 0; i <= expression.length; i++) {
-        if (expression[i] == "(") {
-            inParenthesis++;
-            continue
-        }
-        if (expression[i] == ")") {
-            inParenthesis--;
-            continue;
-        }
-        if ((expression[i] == "+" || expression[i] == "-" || !expression[i]) && inParenthesis == 0) {
-            if (j == 0) {
-                var addend = expression.slice(0, i);
-            } else {
-                var addend = expression.slice(j + 1, i);
-            }
-            if (expression[j] == "+" || j==0) {
-                addends.push((addend));
-            }
-            j = i;
-        }
-    }
-    return addends;
-}
-function subtrahendsOf(expression) {
-    var subtrahends = [];
-    var j = 0;
-    var inParenthesis = 0;
-    for (i = 0; i <= expression.length; i++) {
-        if (expression[i] == "(") {
-            inParenthesis++;
-            continue
-        }
-        if (expression[i] == ")") {
-            inParenthesis--;
-            continue;
-        }
-        if ((expression[i] == "+" || expression[i] == "-" || !expression[i]) && inParenthesis == 0) {
-            if (expression[j] == "-") {
-                var subtrahend = expression.slice(j + 1, i);
-                subtrahends.push((subtrahend));
-            }
-            j = i;
-        }
-    }
-    return subtrahends;
-}
-function noSums(expression) {
-    if (firstAddend(expression) == expression) {
-        return true;
     }
     return false;
 }
-function firstAddend(expression) {
+function expressionTree(expression) {
+    expression = removeUselessParenthesis(expression);
+    if (expression == parseFloat(expression)) {
+        return new Tree(expression);
+    }
+    if (isSum(expression)) {
+        return summationTree(expression)
+
+    }
+    return multiplicationTree(expression)
+}
+function summationTree(expression) {
     var inParenthesis = 0;
-    var addend = "";
-    for (i = 0; i < expression.length - 1; i++) {
-        if (expression[i] == "(") {
-            inParenthesis++;
-            continue
-        }
+    //Parse from right to left
+    for (let i = (expression.length - 1); i >= 0; i--) {
         if (expression[i] == ")") {
+            inParenthesis++;
+            continue;
+        }
+        if (expression[i] == "(") {
             inParenthesis--;
             continue;
         }
-        if ((expression[i] == "+" || (expression[i] == "-" && i != 0)) && inParenthesis == 0) {
-            addend = expression.slice(0, i)
-            return addend;
+        if ((expression[i] == "+" || expression[i] == "-") && inParenthesis == 0) {
+            var tree = new Tree(expression[i])
+            var left = expression.slice(0, i);
+            var leftchild = expressionTree(left)._root;
+            tree._root.children.push(leftchild);
+            tree._root.children[0].parent = tree._root;
+            var right = expression.slice(i + 1);
+            var rightchild = expressionTree(right)._root;
+            tree._root.children.push(rightchild);
+            tree._root.children[1].parent = tree._root;
+            return tree;
         }
     }
-    addend = expression;
-    return addend;
+}
+function multiplicationTree(expression) {
+    var inParenthesis = 0;
+    //Parse from right to left
+    for (let i = (expression.length - 1); i >= 0; i--) {
+        if (expression[i] == ")") {
+            inParenthesis++;
+            continue;
+        }
+        if (expression[i] == "(") {
+            inParenthesis--;
+            continue;
+        }
+        if ((expression[i] == "*" || expression[i] == "/") && inParenthesis == 0) {
+            var tree = new Tree(expression[i])
+            var left = expression.slice(0, i);
+            var leftchild = expressionTree(left)._root;
+            tree._root.children.push(leftchild);
+            tree._root.children[0].parent = tree._root;
+            var right = expression.slice(i + 1);
+            var rightchild = expressionTree(right)._root;
+            tree._root.children.push(rightchild);
+            tree._root.children[1].parent = tree._root;
+            return tree;
+        }
+    }
+}
+function evaluateNode(node) {
+    var data = node.data;
+    if (data == parseFloat(data)) {
+        return parseFloat(data);
+    }
+    if (data == "+" ||
+        data == "*" ||
+        data == "/" ||
+        data == "-") {
+        var left = evaluateNode(node.children[0]);
+        var right = evaluateNode(node.children[1]);
+        return operate(data, left, right);
+    }
 }
 function removeUselessParenthesis(expression) {
     if (expression.startsWith("(")) {
@@ -300,22 +310,6 @@ function operate(operator, x, y) {
     if (operator == "-") { return substract(x, y); }
     if (operator == "*") { return multiply(x, y); }
     if (operator == "/") { return divide(x, y); }
-}
-function sum(array) {
-    var sum = 0;
-    for (let x of array) {
-        x = evaluate(x);
-        sum = add(sum, x);
-    }
-    return sum;
-}
-function product(array) {
-    var product = 1;
-    for (let x of array) {
-        x = evaluate(x);
-        product = product * x;
-    }
-    return product;
 }
 function power(x, y) {
     return x ** y;
